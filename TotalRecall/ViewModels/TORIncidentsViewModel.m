043d18c27c53105c4b0dfd058c5de15141c0d65d
Morten Bøgh <mob@trifork.com>
2014-04-24 09:13:15 +0200

    Incidents are now being cached locally to give a better experience for the user when opening the app.

--- TotalRecall/ViewModels/TORIncidentsViewModel.m
+++ TotalRecall/ViewModels/TORIncidentsViewModel.m
@@ -19,16 +19,29 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
+        self.incidents = [self cachedIncidents];
         PFQuery *query = [PFQuery queryWithClassName:[TORIncident parseClassName]];
         [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
             if (!error) {
                 self.incidents = [objects sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"publishedAt" ascending:NO]]];
+                [self cacheIncidents:self.incidents];
             } else {
                 NSLog(@"Error: %@ %@", error, [error userInfo]);
             }
         }];
     }
     return self;
+}
+
+#pragma mark - Incidents cache
+
+- (NSArray *)cachedIncidents {
+    return [NSKeyedUnarchiver unarchiveObjectWithData:[[NSUserDefaults standardUserDefaults] objectForKey:TORDefaultsIncidentsCache]];
+}
+
+- (void)cacheIncidents:(NSArray *)incidents {
+    [[NSUserDefaults standardUserDefaults] setObject:[NSKeyedArchiver archivedDataWithRootObject:incidents] forKey:TORDefaultsIncidentsCache];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end
