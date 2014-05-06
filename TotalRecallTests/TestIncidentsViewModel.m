80467611be306fbda8df07275274baf856977976
Morten Bøgh <mob@trifork.com>
2014-05-06 20:50:22 +0200

    First test case added to Tilbagekaldt. \o/

---
+++ TotalRecallTests/TestIncidentsViewModel.m
@@ -0,0 +1,39 @@
+//
+//  TestIncidentsViewModel.m
+//  TotalRecall
+//
+//  Created by Morten Bøgh on 06/05/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <Aspects/Aspects.h>
+#import "TORIncidentsViewModel.h"
+
+@interface TestIncidentsViewModel : XCTestCase
+
+@end
+
+@implementation TestIncidentsViewModel
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testIfDownloadLatestIncidentsIsCalled {
+    __block BOOL downloadLatestIncidentsCalled = NO;
+    [TORIncidentsViewModel aspect_hookSelector:@selector(downloadLatestIncidents) withOptions:AspectPositionAfter usingBlock:^(id instance, NSArray *args) {
+        downloadLatestIncidentsCalled = YES;
+    } error:NULL];
+    TORIncidentsViewModel *viewModel = [TORIncidentsViewModel new];
+    XCTAssertFalse(downloadLatestIncidentsCalled, @"downloadLatestIncidents should not be called upon");
+    
+    viewModel.active = YES;
+    XCTAssertTrue(downloadLatestIncidentsCalled, @"downloadLatestIncidents should be called after being activated.");
+}
+
+@end
